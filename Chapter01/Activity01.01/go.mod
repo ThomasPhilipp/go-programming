acbadd6c8a9618a43deb81b809e609707abb388b
Thomas Zwick <thomas.philipp.zwick@gmail.com>
2025-08-30 20:59:25 +0200

    Chapter01/Activity01.01

---
+++ Chapter01/Activity01.01/go.mod
@@ -0,0 +1,3 @@
+module github.com/ThomasPhilipp/go-programming/Chapter01/Activity01.01
+
+go 1.25.0
---
+++ Chapter01/Activity01.01/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstName := "Bob"
+	lastName := "Smith"
+	age := 34
+	peanutAllergy := true
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(age)
+	fmt.Println(peanutAllergy)
+}
