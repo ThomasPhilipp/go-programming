d6f4354f78970923e3a44b6b44f45d7d62ffa2af
Thomas Zwick <thomas.philipp.zwick@gmail.com>
2025-10-02 22:23:43 +0200

    GoDasPraxisBuch/gomd5 add comments

--- GoDasPraxisBuch/gomd5/main.go
+++ GoDasPraxisBuch/gomd5/main.go
@@ -16,11 +16,14 @@ import (
 // $ gomd5 <url>
 // $ gomd5 -h
 
+// Define command-line flags for input sources
 var (
+	// -file flag: specifies a local file to open
 	flagFile = flag.String(
 		"file",
 		"",
 		"File to open")
+	// -url flag: specifies a URL to fetch data from
 	flagUrl = flag.String(
 		"url",
 		"",
@@ -28,33 +31,43 @@ var (
 )
 
 func main() {
+	// Parse the command-line flags provided by the user
 	flag.Parse()
 
+	// Set default input and output streams
 	var input io.Reader = os.Stdin
 	var output io.Writer = os.Stdout
 
+	// Determine the input source based on the flags
 	switch {
 	case *flagFile != "":
+		// If -file flag is provided, try to open the specified file
 		fd, err := os.Open(*flagFile)
 		if err != nil {
 			fmt.Fprintln(output, "Error opening file: ", err)
 			os.Exit(1)
 		}
+		// Ensure the file is closed when done
 		defer fd.Close()
 
+		// Set input to the opened file
 		input = fd
 
 	case *flagUrl != "":
+		// If -url flag is provided, try to fetch the content from the URL
 		resp, err := http.Get(*flagUrl)
 		if err != nil {
 			fmt.Fprintln(output, "Error loading url: ", err)
 			os.Exit(1)
 		}
+		// Ensure the response body is closed when done
 		defer resp.Body.Close()
 
+		// Set input to the response body
 		input = resp.Body
 	}
 
+	// Compute and print the MD5 hash of the input data
 	printMD5(input, os.Stdout)
 }
 
